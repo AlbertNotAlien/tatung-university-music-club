92c3df6ee38973a1a348a165850e2231576e2c14
AlbertNotAlien <albert830207@gmail.com>
2024-08-10 23:50:21 +0800

    fix: fix incorrect Firestore path

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BookingCalendar } from '@/components/booking-calendar';
-import { getEvents } from '@/api/firebase/firestore';
+import { getEvents } from '@/lib/firebase/firestore';
 
 export default async function Page() {
   const events = await getEvents();
