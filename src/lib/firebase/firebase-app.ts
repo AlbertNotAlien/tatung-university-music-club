b54e119e7f8c0a58954b5f82cb4b50173db63ed8
AlbertNotAlien <albert830207@gmail.com>
2024-08-28 02:50:27 +0800

    feat: install firebase/auth

--- src/lib/firebase/firebase-app.ts
+++ src/lib/firebase/firebase-app.ts
@@ -1,5 +1,5 @@
 import { initializeApp, getApp, getApps } from 'firebase/app';
-import { initializeApp, getApps } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -15,6 +15,5 @@ const firebaseConfig = {
 export const firebaseApp = getApps().length
   ? getApp()
   : initializeApp(firebaseConfig);
-export const firebaseApp =
-  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+export const firebaseAuth = getAuth(firebaseApp);
 export const db = getFirestore(firebaseApp);
