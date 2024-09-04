9b7048495a545636ec03c6c2095de57dbcab8020
StanleyHaw <stanleyhaw94@gmail.com>
2024-09-04 13:05:16 +0800

    feat: add teacher cards section with props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+import React, { useId } from 'react';
 import TeacherCard from '@/components/teacher-card';
-import React from 'react';
+import { teacherList } from '@/data/teacher-list';
+
+function TeacherCardsSection() {
+  const id = useId();
+
+  return (
+    <section className="no-scrollbar container overflow-x-scroll">
+      <div className="flex w-[2000px] flex-row">
+        {teacherList.map((teacher) => {
+          return <TeacherCard key={`${teacher.name}_${id}`} {...teacher} />;
+        })}
+      </div>
+    </section>
+  );
+}
 
 export default function Home() {
-  return (
-    <TeacherCard />
-  );
+  return <TeacherCardsSection />;
 }
