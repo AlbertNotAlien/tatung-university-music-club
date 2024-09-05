cf2ce7c3d67ce5dc343634aea17dbe3a535885e1
Apple User-2 <apple-2@AppledeiMac-2.local>
2024-09-05 10:43:24 +0800

    feat: change scrollable wrapper for teacher cards instead of carousel

--- src/components/teacher-introduction-section.tsx
+++ src/components/teacher-introduction-section.tsx
@@ -1,33 +1,14 @@
 import React from 'react';
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from '@/components/ui/carousel';
 import TeacherCard from '@/components/teacher-card';
 import { teacherList } from '@/data/teacher-list';
-import { cn } from '@/lib/utils';
 
 export default function TeacherIntroductionSection() {
   return (
-    <Carousel>
-      <CarouselContent className="ml-1 flex flex-row">
-        {teacherList.map((teacher) => {
-          return (
-            <CarouselItem
-              key={`${teacher.id}`}
-              className={cn('basis-5/6 px-2 py-6', 'md:basis-[360px] md:px-0')}
-            >
-              <TeacherCard {...teacher} />
-            </CarouselItem>
-          );
-        })}
-      </CarouselContent>
-      <CarouselNext className="-right-2" />
-      <CarouselPrevious className="-left-2" />
-    </Carousel>
+    <section className="no-scrollbar flex snap-x snap-mandatory flex-row overflow-y-scroll">
+      {teacherList.map((teacher) => (
+        <TeacherCard key={teacher.id} {...teacher} />
+      ))}
+    </section>
   );
 }
 
