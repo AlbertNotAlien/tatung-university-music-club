fbb980b86316144231c0b8198c613050e34f25b3
DannyChen <dannychen@kkcompany.com>
2024-11-09 20:45:41 +0800

    feat(utils): add util function to get bookings date range

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const getCurrentWeekRange = (): [startDate: Date, endDate: Date] => {
+  const today = new Date();
+
+  // 計算當週的星期日
+  const sunday = new Date(today);
+  sunday.setDate(today.getDate() - today.getDay());
+
+  // 計算當週的星期六
+  const saturday = new Date(sunday);
+  saturday.setDate(sunday.getDate() + 6);
+
+  return [sunday, saturday];
+};
